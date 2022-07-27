95d5c8960f4927a947edc7328b6529389d5e53f0
Ana Caroline <caroline.ana.n.s@gmail.com>
2022-07-27 03:14:34 +0000

    Feat: Introdução ao html e css

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 ##app se chama app pq é uma convenção
 ##que argumento eu passo pro flask? O nome da aplicação
 app = Flask("hello")
+posts = [
+    {
+        "title": "First post",
+        "body": "Post content",
+        "author": "Vinicius",
+        "created": datetime(2022, 7, 25)
+    },
+    {
+        "title": "Second post",
+        "body": "Post content",
+        "author": "Jubileu",
+        "created": datetime(2022, 7, 26)
+    }
+]
+
 
 ##estou criando minha primeira rota
 @app.route("/")
@@ -15,3 +31,12 @@ def hello():
 def meuContato():
     return render_template('index.html')
 
+@app.route("/")
+def index():
+    return render_template('index.html', 
+                            posts=posts
+                            )
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
