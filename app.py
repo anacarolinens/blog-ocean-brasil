e92bc31917233170109aa39dbce5a34819930701
Ana Caroline <caroline.ana.n.s@gmail.com>
2022-07-28 00:36:56 +0000

    Feat: Final da aula 4

--- app.py
+++ app.py
@@ -1,42 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-##app se chama app pq é uma convenção
-##que argumento eu passo pro flask? O nome da aplicação
 app = Flask("hello")
-posts = [
-    {
-        "title": "First post",
-        "body": "Post content",
-        "author": "Vinicius",
-        "created": datetime(2022, 7, 25)
-    },
-    {
-        "title": "Second post",
-        "body": "Post content",
-        "author": "Jubileu",
-        "created": datetime(2022, 7, 26)
-    }
-]
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
 
 
-##estou criando minha primeira rota
-@app.route("/")
-@app.route("/hello")
-##CRIA UMA FUNÇÃO QUE RETORNA HELLO WORLD
-def hello():
-    return "Hello World"
-
-@app.route("/meucontato")
-def meuContato():
-    return render_template('index.html')
+db.create_all()
 
 @app.route("/")
 def index():
-    return render_template('index.html', 
-                            posts=posts
-                            )
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
+@app.route("/populate")
+def populate():
+    user = User(username='Ana', email="g@g.com", password_hash='a')
+    post1 = Post(title="Post 1", body="Texto do Post", author=user)
+    post2 = Post(title="Post 2", body="Texto do Post 2", author=user)
+    db.session.add(user)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    return redirect(url_for('index'))
